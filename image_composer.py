f61f1963066e2d7f8d6c4ac84380644481e4733e
paulodiff <usercr@CLI_1699.ad.comune.rimini.it>
2018-10-25 11:52:59 +0200

    25/10/2018-11:52:59,09

--- image_composer.py
+++ image_composer.py
@@ -1,6 +1,49 @@
 # open an image and insert another image into
 
 from PIL import Image
+import glob
+import os
+import numpy as np
+import cv2
+
+img_source_folder = 'c:/nn/dataset/mazzo_bicycle'
+img_out_folder = 'c:/nn/dataset/dest'
+num_immagini_da_generare = 200 # genera x immagini per ogni immagine presente in source
+write_boxed_images = False # write image con box per test
+xml_database_info = 'BurracoPoints'
+
+
+file_list = os.listdir(img_source_folder)
+
+images_list = []
+
+for j, item in enumerate(file_list):
+  
+  # filtra solo le immagini 
+  if item.lower().endswith(('.png', '.jpg')):
+    img_path = img_source_folder + '/' + item
+    images_list.append(img_path)
+
+n_pic = 0
+s_w = 0
+s_h = 0
+
+for j, img_path in enumerate(images_list):
+ 
+    print('img_path:', img_path)
+    img = Image.open(img_path, 'r')
+    img_w, img_h = img.size
+    s_h = s_h + img_h
+    s_w = s_w + img_w
+    print(img.size)
+    n_pic = n_pic + 1
+    
+
+m_size = ((s_w) // n_pic, (s_h) // n_pic)  
+
+print(m_size)
+
+'''
 img = Image.open('C:/nn/dataset/tmp/10Q.jpg', 'r')
 img_w, img_h = img.size
 print(img.size)
@@ -18,3 +61,4 @@ print(offset)
 
 background.paste(img, offset)
 background.save('C:/nn/sample_imgs/out.jpg')
+'''
